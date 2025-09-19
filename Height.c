d9c7d6649e74f410c2158b38dbe91f65734d8ab7
v9goblin <1420122@gmail.com>
2025-09-19 16:15:10 +0300

    Add files via upload

---
+++ Height.c
@@ -0,0 +1,25 @@
+/*
+Name:James Githahu
+Reg no:CT101/G/26463/25 
+Description:height, bank balance, phone number
+*/
+#include<stdio.h>
+
+int main()
+{
+    int height;
+    printf("enter your height:");
+    scanf("%d", & height);
+    printf("your height is %d\n", height);
+    
+    int balance;
+    printf("enter your balance:");
+    scanf("%d", &balance );
+    printf("your balance is %d\n", balance);
+    
+    int number;
+    printf("enter your number");
+    scanf("%d", & number);
+    printf("your number is %d", number);
+    return 0;
+}
---
+++ Units.c
@@ -0,0 +1,19 @@
+/*
+Name:James Githahu
+Reg no:CT101/G/26463/25 
+Description:Units
+*/
+#include<stdio.h>
+
+int main()
+{
+    printf("introduction to programming and problem solving \n");
+    printf("introduction to computer architecture \n");
+    printf("physics\n");
+    printf("discrete mathematics\n");
+    printf("physics\n");
+    printf("mathematics for science\n");
+    printf("communication skills\n");
+    printf("analogue electronics ");
+    return 0;
+}
